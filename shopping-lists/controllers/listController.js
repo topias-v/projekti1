300cbee927376732cc5848f304e01dd7a16470d8
topias <topias.vesanen@hotmail.com>
2023-10-30 16:55:41 +0200

    updated library versions.

--- shopping-lists/controllers/listController.js
+++ shopping-lists/controllers/listController.js
@@ -1,4 +1,4 @@
-import { renderFile } from "https://deno.land/x/eta@v2.0.0/mod.ts";
+import { renderFile } from "https://deno.land/x/eta@v2.2.0/mod.ts";
 import * as listService from "../services/listService.js";
 import * as itemService from "../services/itemService.js";
 import * as requestUtils from "../utils/requestUtils.js";
